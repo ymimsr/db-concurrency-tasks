d64474db434e87de25e8f59483cbc05ab4fd9b72
ymim <43496855+ymimsr@users.noreply.github.com>
2022-10-12 16:18:04 +0700

    task_16 impl

---
+++ task_16/src/main/java/com/ymimsr/task_16/Main.java
@@ -0,0 +1,43 @@
+package com.ymimsr.task_16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            URL url = new URL(args[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            try (
+                    BufferedReader httpReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))
+            ) {
+                String line;
+                int i = 0;
+                while ((line = httpReader.readLine()) != null) {
+                    if (i == 25) {
+                        i = 0;
+                        System.out.println("Press any return to scroll down.");
+                        consoleReader.readLine();
+                    }
+
+                    i++;
+                    System.out.println(line);
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            } finally {
+                connection.disconnect();
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+}
