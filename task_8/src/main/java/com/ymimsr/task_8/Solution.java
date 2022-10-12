6282a244bea9d7c1f8ac91e9cb8139c2f5ddcbb0
ymim <43496855+ymimsr@users.noreply.github.com>
2022-10-12 14:45:19 +0700

    task_8 refactoring

--- task_8/src/main/java/com/ymimsr/task_8/Solution.java
+++ task_8/src/main/java/com/ymimsr/task_8/Solution.java
@@ -1,8 +1,5 @@
 package com.ymimsr.task_8;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
